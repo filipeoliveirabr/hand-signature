bf0e41680274f854c005637c705ff1b6f5596585
Filipe <filipeoliveira.php@gmail.com>
2018-03-05 12:30:57 -0300

    Assinaturas aleatorias de exemplo

--- segmentation.py
+++ segmentation.py
@@ -2,6 +2,7 @@ import cv2
 import parser_polygon
 import util
 import polygon as Polygon
+from matplotlib import pyplot as plt
 
 def print_line_from_polygon(image, polygon):
     index = 0
@@ -15,20 +16,20 @@ def print_line_from_polygon(image, polygon):
             
         index = index + 1
 
-image     = cv2.imread('./examples/signature2.png')
+image     = cv2.imread('./examples/cut/1.jpg')
 gray      = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY) 
 blurred   = cv2.GaussianBlur(gray,(9,9),0)
-threshold = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,11,1)
+
+th3 = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,2)
+
+threshold = cv2.adaptiveThreshold(blurred,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY, 11,1)
 
 (_, contours,_) = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-
 
 polygons = []
 for contour in contours:
     hull = cv2.convexHull(contour)
     polygons.append( parser_polygon.points_to_polygon(hull) )
-
 
 polygons_aux = []
 
@@ -49,16 +50,21 @@ for polygon_a in polygons_aux:
     if not( is_inside ):
         polygons.append( polygon_a )
 
-for polygon in polygons:
-    print_line_from_polygon(image, polygon)
-    
-cv2.imshow("Image", image)    
-cv2.waitKey()
-    
-    #if( util.is_point_inside_poly(x, y, i) ):
-    #    print(x + " " + y)
- 
-#print( util.is_point_inside_poly(point_x, point_y, polygon))
-    
-#cv2.imshow("Image", image)    
-#cv2.waitKey()
+#for polygon in polygons:
+    #print_line_from_polygon(image, polygon)
+
+
+cv2.imshow("Gray", gray)
+cv2.waitKey(0)
+
+
+cv2.imshow("Image", image)
+cv2.waitKey(0)
+
+#plt.figure(1)
+#plt.subplot(212)
+#plt.imshow(gray)
+
+#plt.subplot(211)
+#plt.imshow(image)
+#plt.show()
